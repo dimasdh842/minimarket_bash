65be5a1062546111b9c5a23f682877f5ba8c584c
dimasdh842 <penamas842@gmail.com>
2021-12-17 21:33:56 +0700

    add some menus

--- index.sh
+++ index.sh
@@ -5,72 +5,81 @@ echo "Kelompok 22"
 
 declare -a pilihan
 declare -A menus
-menus[0,0]=1
-menus[0,1]=10
-menus[0,2]="beras 12kg"
+menus[1,0]=1
+menus[1,1]=10
+menus[1,2]="beras 12kg"
 
-menus[1,0]=2
-menus[1,1]=5
-menus[1,2]="Mizone 1 liter"
+menus[2,0]=2
+menus[2,1]=5
+menus[2,2]="Mizone 1 liter"
 
-menus[2,0]=3
-menus[2,1]=25
-menus[2,2]="Pampers L 40"
+menus[3,0]=3
+menus[3,1]=25
+menus[3,2]="Pampers L 40"
 
-menus[3,0]=4
-menus[3,1]=100
-menus[3,2]="Minyak Kayu Putih 1 liter"
+menus[4,0]=4
+menus[4,1]=100
+menus[4,2]="Minyak Kayu Putih 1 liter"
 
-menus[4,0]=5
-menus[4,1]=70
-menus[4,2]="Mainan Kayu Putih 2 liter"
+menus[5,0]=5
+menus[5,1]=70
+menus[5,2]="Mainan Kayu Putih 2 liter"
 
-menus[5,0]=6
-menus[5,1]=21
-menus[5,2]="Minyak Kayu Putih 3 liter"
+menus[6,0]=6
+menus[6,1]=21
+menus[6,2]="Minyak Kayu Putih 3 liter"
 
-menus[6,0]=7
-menus[6,1]=41
-menus[6,2]="Mainan Bayi"
+menus[7,0]=7
+menus[7,1]=41
+menus[7,2]="Mainan Bayi"
 
-menus[7,0]=8
-menus[7,1]=51
-menus[7,2]="Mainan Anak"
+menus[8,0]=8
+menus[8,1]=51
+menus[8,2]="Mainan Anak"
 
-menus[8,0]=9
-menus[8,1]=61
-menus[8,2]="gulaku 12kg"
+menus[9,0]=9
+menus[9,1]=61
+menus[9,2]="gulaku 12kg"
 
-menus[9,0]=10
-menus[9,1]=50
-menus[9,2]="Minyak Goreng Palmia 1 liter"
+menus[10,0]=10
+menus[10,1]=50
+menus[10,2]="Minyak Goreng Palmia 1 liter"
 
-menus[10,0]=11
-menus[10,1]=30
-menus[10,2]="Aqua botol 2liter"
-
-menus[11,0]=12
+menus[11,0]=11
 menus[11,1]=30
-menus[11,2]="pampers s 40"
+menus[11,2]="Aqua botol 2liter"
 
-menus[12,0]=13
-menus[12,1]=35
-menus[12,2]="Minyak kayu putih 7 liter"
+menus[12,0]=12
+menus[12,1]=30
+menus[12,2]="pampers s 40"
 
-menus[13,0]=14
-menus[13,1]=342
-menus[13,2]="notebook"
+menus[13,0]=13
+menus[13,1]=35
+menus[13,2]="Minyak kayu putih 7 liter"
 
-menus[14,0]=15
-menus[14,1]=42
-menus[14,2]="bulpoint standard 1 pack"
+menus[14,0]=14
+menus[14,1]=342
+menus[14,2]="notebook"
 
-menus[15,0]=16
-menus[15,1]=48
-menus[15,2]="Masker 20 pack"
+menus[15,0]=15
+menus[15,1]=42
+menus[15,2]="bulpoint standard 1 pack"
+
+menus[16,0]=16
+menus[16,1]=48
+menus[16,2]="Masker 20 pack"
+
+menus[17,0]=17
+menus[17,1]=24
+menus[17,2]="Masker 10 pack"
+
+menus[18,0]=18
+menus[18,1]=12
+menus[18,2]="Masker 5 pack"
 
 length=${#menus[@]}
 lengthbaru=$(expr $length / 3)
+
 
 echo "==========================================";
 
@@ -120,19 +129,23 @@ echo " 15) bulpoint standard 1 pack";
 
 echo " 16) Masker 20 Pack ";
 
-echo " 17) Keluar ";
+echo " 17) masker 10 pack";
+
+echo " 18) Masker 5 Pack ";
+
+echo " 19) Keluar ";
 
 
 echo "==========================================";
 
 
 pil=0
-while [ $pil -le 16 ]
+while [ $pil -le 18 ]
 do
-        echo -n " Masukkan Pilihan Menu Anda $nama [1-17] : ";
+        echo -n " Masukkan Pilihan Menu Anda $nama [1-19] : ";
         read pil
 
-        if [ $pil -ne 17 ];
+        if [ $pil -ne 19 ];
         then
         pilihan+=($pil)   
         else
@@ -142,7 +155,7 @@ done
 
 
 
-echo "${pilihan[1]}"
+# echo "${pilihan[0]}"
 
 length=${#pilihan[@]}
 echo "==========================================";
